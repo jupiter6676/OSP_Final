570b96abc5c8487981cf2bbe8e0898655e546b92
selenee623 <62587855+selenee623@users.noreply.github.com>
2020-06-26 20:43:04 +0900

    Update app.py
    단일 url 입력은 되지만 txt 입력은 오류 있음
    코사인 유사도 출력 xxx

--- app.py
+++ app.py
@@ -1,197 +1,267 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-
 import re
-import requests
-from bs4 import BeautifulSoup
 import sys
-from flask import Flask, request, render_template
-from nltk import word_tokenize
-import numpy
-
+import requests
+import numpy as np
+from bs4 import BeautifulSoup
+from flask import Flask, request, render_template, url_for
 
 app = Flask(__name__, static_url_path='/static')
 
+# 전역 변수
+word_d = {}
+sent_list = []
+
+
+# 특수문자 제거 (공백 제외)
+def clean_word_list(input_list):
+    output_list = []
+
+    for word in input_list:
+        symbols = """!•@#$%^&*()_-+={[}]|\\;:"‘'·<>?/., """
+
+        for i in range(len(symbols)):
+            word = word.replace(symbols[i], '')
+
+        if len(word) > 0 :
+            output_list.append(word)
+
+    return output_list
+
+
+# { '단어' : '빈도수' }
+def process_new_sentence(input_list):
+	sent_list.append(input_list)
+
+	for word in input_list:
+
+		if word in word_d:
+			word_d[word] += 1
+
+		else:
+			word_d[word] = 0
+
+	return word_d
+
+
+# 길이가 같은 벡터 만들기
+def make_vector(i, input_list):
+	v = []
+	s = sent_list[i]
+
+	for word in word_d.keys():
+
+		val = 0
+
+		for t in input_list:
+			if t == word:	val += 1
+
+		v.append(val)
+
+	return v
+
+
+# 시작 화면
 @app.route('/')
-
 def index():
-    return render_template('page.html')
+    return render_template('home.html')
 
 
-@app.route('/box', methods=['GET'])
+# # 단일 URL 입력
+# @app.route('/box', methods=['POST'])
+# def box():
+#     myurl = request.form['wa']
 
+#     res = requests.get(myurl)
 
-def read_url():
+#     html = BeautifulSoup(res.content, 'html.parser')
 
+#     body = html.select('body')
 
-	# 특수문자 제거
-	def clean_word_list(input_list):
+#     word = []
 
-		output_list = []
+#     for tag in body:
+#         word.append(tag.get_text().strip())
 
-		for word in input_list:
+#     # freq
+#     word_list = []
+#     word_list = ''.join(word).lower().split()
 
-			symbols = """!•@#$%^&*()_-+={[}]|\;:"‘'·<>?/., """
+#     clean_list = []
+#     clean_list = clean_word_list(word_list)
 
-			for i in range(len((symbols))):
-				word = word.replace(symbols[i], '')
+#     # { '단어' : '빈도수' }
+#     def counter(input_list):
+#         word_count = {}
 
-			if len(word) > 0:
-				output_list.append(word)
+#         for word in input_list:
+#             if word in word_count:
+#                 word_count[word] += 1
+#             else:
+#                 word_count[word] = 1
 
-		return output_list
+#         return word_count
+    
+#     word_count = counter(clean_list)
+#     word_count = sorted(word_count.items(), key=lambda x:x[1], reverse=True)
 
+#     count = 0
 
-	# { '단어' : '빈도수' }
-	def counter(input_list):	# process_new_sentence(s)
+#     w_list = []
+#     f_list = []
+    
+#     while count < 30:
+#         w_list.append(word_count[count][0])
+#         f_list.append(word_count[count][1])
 
-		word_count = {}
-			
-		for word in input_list:
+#         count = count + 1
 
-			if word in  word_count:
+#     success = "success"
 
-				word_count[word] += 1
+#     return render_template('page.html', wa=myurl, word_list=w_list, freq=f_list, wc=word_count, status=success)
 
-			else:
-				word_count[word] = 1
 
-		return word_count
-
-
-
-	if request.method == 'GET':
-
-		url = requests.get("https://nutch.apache.org/")
-		html = BeautifulSoup(url.content, 'html.parser')
-
-
-		body = html.select('body')
-
-		word = []	# 맨 처음 크롤링한 데이터
-
-
-		for tag in body:
-
-			word.append(tag.get_text().strip())
-
-
-		"""
-		freq
-		"""
-
-		word_list = []
-
-		word_list = ''.join(word).lower().split()	# word를 문자열로 변환 후, 공백 기준으로 나누기
-
-
-
-			
-
-		clean_list = []
-
-		clean_list = clean_word_list(word_list)	# 특수 문자 제거
-
-
-
-
-
-		word_count = counter(clean_list)	# 딕셔너리
-
-		word_count = sorted(word_count.items(), key=lambda x:x[1], reverse=True)	# 정렬 후 튜플
-
-
-
-		count = 0
-
-		w_list = []
-		f_list = []
-
-		while count < 30:
-
-			w_list.append(word_count[count][0])
-			f_list.append(word_count[count][1])
-
-			count = count + 1
-
-
-
-
-
-		url_2 = requests.get("http://attic.apache.org/")
-		html_2 = BeautifulSoup(url_2.content, 'html.parser')
-
-
-		body_2 = html_2.select('body')
-
-		word_2 = []	# 맨 처음 크롤링한 데이터
-
-
-		for tag in body_2:
-
-			word_2.append(tag.get_text().strip())
-
-
-		"""
-		freq
-		"""
-
-		word_list_2 = []
-
-		word_list_2 = ''.join(word_2).lower().split()	# word를 문자열로 변환 후, 공백 기준으로 나누기
-
-
-
-			
-
-		clean_list_2 = []
-
-		clean_list_2 = clean_word_list(word_list_2)	# 특수 문자 제거
-
-
-
-
-
-		word_count_2 = counter(clean_list_2)	# 딕셔너리
-
-		word_count_2 = sorted(word_count_2.items(), key=lambda x:x[1], reverse=True)	# 정렬 후 튜플
-
-
-
-		count = 0
-
-		w_list_2 = []
-		f_list_2 = []
-
-		while count < 30:
-
-			w_list_2.append(word_count_2[count][0])
-			f_list_2.append(word_count_2[count][1])
-
-			count = count + 1
-
-
-
-
-		dotpro = numpy.dot(f_list, f_list_2)
-		cosSimil = dotpro / (norm(f_list) * norm(f_list_2))
-	
-		"""
-		res = requests.args.get('https://github.com/trending/python?since=monthly')
-		#https://github.com/trending/python?since=monthly
-		"""
-		return render_template('page.html', url1 = url, word_list = w_list, freq = f_list, cos = cosSimil)
-
-
+# txt 파일 입력
 @app.route('/txt', methods=['POST'])
+def txt():
+    # 주소 받는 거
+    # # submit 시 request 된 파일 데이터 처리
+    # f = request.files['file']
 
-def read_txt():
+    # # 이렇게 하면 앞에 b' 뒤에 \n' 이 붙어서 나옴
+    # myurl2 = []
+    # for lines in f:
+    #     myurl2.append(lines)
 
-    if request.method == 'POST':
-        f = request.files['file'].read()
+    # return render_template('home.html', file=myurl2)
 
-        return f
+    #1 기준
+    url = requests.get("https://nutch.apache.org/")
+    html = BeautifulSoup(url.content, 'html.parser')
+
+    body = html.select('body')
+
+    word = []
+
+    for tag in body:
+        word.append(tag.get_text().strip())
+    
+    # word를 문자열로 변환 후, 공백 기준으로 나누기
+    word_list = []
+    word_list = ''.join(word).lower().split
+
+    clean_list = []
+    clean_list = clean_word_list(word_list)
+
+    process_new_sentence(clean_list)
+
+
+    #2
+    url_2 = requests.get("http://attic.apache.org/")
+    html_2 = BeautifulSoup(url_2.content, 'html.parser')
+
+    body_2 = html_2.select('body')
+
+    word_2 = []	# 맨 처음 크롤링한 데이터
+
+    for tag in body_2:
+        word_2.append(tag.get_text().strip())
+
+    word_list_2 = []
+    word_list_2 = ''.join(word_2).lower().split()	# 공백 기준으로 나누기
+
+    clean_list_2 = []
+    clean_list_2 = clean_word_list(word_list_2)	# 특수 문자 제거
+
+    process_new_sentence(clean_list_2)
+
+
+    # 3
+    url_3 = requests.get("https://brooklyn.apache.org/")
+    html_3 = BeautifulSoup(url_3.content, 'html.parser')
+
+    body_3 = html_3.select('body')
+
+    word_3 = []	# 맨 처음 크롤링한 데이터
+
+    for tag in body_3:
+        word_3.append(tag.get_text().strip())
+
+    word_list_3 = []
+    word_list_3 = ''.join(word_3).lower().split()	# 공백 기준으로 나누기
+
+    clean_list_3 = []
+    clean_list_3 = clean_word_list(word_list_3)	# 특수 문자 제거
+
+    process_new_sentence(clean_list_3)
+
+
+    # 4
+    url_4 = requests.get("https://allura.apache.org/")
+    html_4 = BeautifulSoup(url_4.content, 'html.parser')
+
+    body_4 = html_4.select('body')
+
+    word_4 = []	# 맨 처음 크롤링한 데이터
+
+    for tag in body_4:
+        word_4.append(tag.get_text().strip())
+
+    word_list_4 = []
+    word_list_4 = ''.join(word_4).lower().split()	# 공백 기준으로 나누기
+
+    clean_list_4 = []
+    clean_list_4 = clean_word_list(word_list_4)	# 특수 문자 제거
+
+    process_new_sentence(clean_list_4)
+
+
+    # 벡터 길이 같게 하기 위해 마지막에 생성
+    #print(sent_list[0])
+    v1 = np.array(make_vector(0, clean_list))
+    #print(v1)
+
+
+    #print(sent_list[1])
+    v2 = np.array(make_vector(1, clean_list_2))
+    #print(v2)
+
+
+    #print(sent_list[2])
+    v3 = np.array(make_vector(2, clean_list_3))
+    #print(v3)
+
+
+    #print(sent_list[3])
+    v4 = np.array(make_vector(3, clean_list_4))
+    #print(v4)
+
+    dotpro_1 = np.dot(v1, v2)
+    cosSimil_1 = dotpro_1 / (sum(v1**2) * sum(v2**2))
+
+    #print(dotpro_1)
+    #print("Cosine Similarity (v1, v2) : ", cosSimil_1)
+
+    dotpro_2 = np.dot(v1, v3)
+    cosSimil_2 = dotpro_2 / (sum(v1**2) * sum(v3**2))
+
+    #print(dotpro_2)
+    #print("Cosine Similarity (v1, v3) : ", cosSimil_2)
+
+    dotpro_3 = np.dot(v1, v4)
+    cosSimil_3 = dotpro_3 / (sum(v1**2) * sum(v4**2))
+
+    #print(dotpro_3)
+    #print("Cosine Similarity (v1, v4) : ", cosSimil_3)
+    return render_template('home.html', simi1 = cosSimil_1, simi2 = cosSimil_2, simi3 = cosSimil_3)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    # with open(url_for('static'), filename=file) as f:
+    #     for line in f:
+    #         str = line
+    #         arr = str.split('\n')
